406221c37c31bf3ffd048a5da7090414cb527f46
Arief111 <96244303+Arief111@users.noreply.github.com>
2021-12-17 16:48:06 +0700

    Update UAS Prokom Final.py

--- UAS Prokom Final.py
+++ UAS Prokom Final.py
@@ -4,4 +4,4 @@ import streamlit as st
 st.title("Welcome to Streamlit")
 st.write("Here is our first data frame")
 
-st.write(pd.DataFrame({"Nama":["Arief","Maulana"],"Pacar":["Quynh", "Hoa"]})
+st.write(pd.DataFrame({'Nama':["Arief","Maulana"],'Pacar':["Quynh", "Hoa"]})
