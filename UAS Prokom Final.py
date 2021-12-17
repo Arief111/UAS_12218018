21925e004764b9c238cb142d286082edf9fd68a2
Arief111 <96244303+Arief111@users.noreply.github.com>
2021-12-17 16:55:14 +0700

    Update UAS Prokom Final.py

--- UAS Prokom Final.py
+++ UAS Prokom Final.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Welcome to Streamlit")
+st.title("Welcome to Streamlit!")
 
-st.write("Here is our first data frame")
+st.write("Our first DataFrame")
 
 st.write(pd.DataFrame({'A': [1, 2, 3, 4],'B': [5, 6, 7, 8]}))
+#Kotnr
