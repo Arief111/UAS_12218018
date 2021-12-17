f2290ec71b8e4f3e60558f8f8ab05f51d84236d3
Arief111 <96244303+Arief111@users.noreply.github.com>
2021-12-17 16:57:52 +0700

    Update python1.py

--- python1.py
+++ python1.py
@@ -5,5 +5,5 @@ st.title("Welcome to Streamlit!")
 
 st.write("Our first DataFrame")
 
-st.write(pd.DataFrame({'A': [1, 2, 3, 4],'B': [5, 6, 7, 8]}))
+st.write(pd.DataFrame({'Saya': ["Arief"],'Pacar': ["Hoa"]}))
 #Kotnr
