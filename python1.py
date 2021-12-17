28cbb8d3ee14d9250ab6f58dfa5060fafcfe1d88
Arief111 <96244303+Arief111@users.noreply.github.com>
2021-12-17 18:11:56 +0700

    Update python1.py

--- python1.py
+++ python1.py
@@ -176,7 +176,7 @@ if(Token == 1):
 
 elif(Token == 2): 
     Ranking = st.number_input("Silahkan Masukkan Berapa Negara : ")
-    Tahun = st.number_input("Silahkan Masukkan Tahun : "))
+    Tahun = st.number_input("Silahkan Masukkan Tahun : ")
     DataFrameTahun = HandleFileProduksi1.loc[HandleFileProduksi1["Tahun"]==Tahun]
     DataFrameTahunSort = DataFrameTahun.sort_values(["Produksi"], ascending=[0])
     st.write(DataFrameTahunSort[0:Ranking])
